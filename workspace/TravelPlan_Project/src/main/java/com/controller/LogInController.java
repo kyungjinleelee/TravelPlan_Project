71d84ced3d1ffbca85e9f67dfa71489c42497e39
ParkSeolah <paouoeq@naver.com>
2023-10-18 16:58:41 +0900

    로그인폼 구현(로그인, 로그인 실패), top/main jsp 임시구현

---
+++ workspace/TravelPlan_Project/src/main/java/com/controller/LogInController.java
@@ -0,0 +1,46 @@
+package com.controller;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dto.MemberDTO;
+import com.service.MemberService;
+
+@Controller
+public class LogInController {
+
+	@Autowired
+	MemberService service;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam HashMap<String, String> map, HttpSession session) {
+		
+		MemberDTO dto = service.login(map);
+		System.out.println(dto);
+		
+		if(dto!=null) { // 로그인 성공
+			session.setAttribute("loginInfo", dto); // 로그인정보 저장
+			return "main";			
+		}
+		else {
+			return "member/loginFail";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		return "redirect:main";
+	}
+}
