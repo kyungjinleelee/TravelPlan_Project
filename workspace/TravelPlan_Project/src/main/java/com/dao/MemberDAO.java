ad2839d9fbf4de33a546c1ee4a9afc4342097f60
ParkSeolah <paouoeq@naver.com>
2023-10-18 17:41:31 +0900

    로그인폼 구현(로그인, 로그인 실패)2

--- workspace/TravelPlan_Project/src/main/java/com/dao/MemberDAO.java
+++ workspace/TravelPlan_Project/src/main/java/com/dao/MemberDAO.java
@@ -1,13 +1,20 @@
 package com.dao;
 
+import java.util.HashMap;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.dto.MemberDTO;
 
 @Repository("dao")
 public class MemberDAO {
 	@Autowired // SqlSessionTemplate 주입
 	SqlSessionTemplate session;
 	
-	
+	// 로그인
+	public MemberDTO login(HashMap<String, String> map) {
+		return session.selectOne("MemberMapper.login", map);
+	}
 }
