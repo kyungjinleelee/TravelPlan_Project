251cc3d36158cd3e5dba714cf780d93e04f47b69
ParkSeolah <paouoeq@naver.com>
2023-10-19 13:24:44 +0900

    [Feat] 추가 로그아웃 기능
    - 인터셉터 로그인 체크 기능 추가(LoginCheckHandlerInterceptor)
    - 로그아웃 기능 추가(LoginController)

--- workspace/TravelPlan_Project/src/main/java/com/interceptor/LoginCheckHandlerInterceptor.java
+++ workspace/TravelPlan_Project/src/main/java/com/interceptor/LoginCheckHandlerInterceptor.java
@@ -2,33 +2,41 @@ package com.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-@Component("xxx")
+import com.dto.MemberDTO;
+
+@Component("loginCheckInterceptor")
 public class LoginCheckHandlerInterceptor extends HandlerInterceptorAdapter  {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return super.preHandle(request, response, handler);
+		
+		// 로그인 체크 여부 확인
+		HttpSession session = request.getSession();
+		MemberDTO dto = (MemberDTO)session.getAttribute("loginInfo"); // 세션에 있는 login정보 저장
+		
+		if(dto == null) {// 저장이 안된 경우 -> 로그인 X
+			response.sendRedirect("/app/loginForm"); // 로그인하도록 유도
+			return false; // false이기  때문에 interceptor가 적용된 Controller는 실행되지 않음
+		}
+		return true; // null값이 아니라면 dto에 로그인 정보 저장 -> 로그인 O
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		super.postHandle(request, response, handler, modelAndView);
+		
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
-		super.afterCompletion(request, response, handler, ex);
 	}
 	
 }
